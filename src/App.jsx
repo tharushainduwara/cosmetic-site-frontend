df6d9c5b6f7971a9f7967a5b6108c2c93be32715
Tharusha <tharushaiduwara1234@gmail.com>
2026-01-16 17:36:20 +0530

    Frontend 01

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+
+  return (
+    <>
+    <ProductCard name= "Apple iPad" price="$220"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard(props){
+
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <p>{props.price}</p>
+            <button>Add to Cart</button>
+        </div>
+    )
+
+}
