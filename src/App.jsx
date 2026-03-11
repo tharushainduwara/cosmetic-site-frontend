68591cc7b717acd2ec6906b3a37dd5b0f506fc7b
Tharusha <tharushaiduwara1234@gmail.com>
2026-03-11 17:23:28 +0530

    account settings enabled

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Toaster } from 'react-hot-toast';
 import RegisterPage from './pages/registerPage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import ForgetPassword from './pages/forget-password';
+import UserSettings from './pages/settings';
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
           <Route path ="/login" element={<LoginPage/>}/>
           <Route path = "/admin/*" element={<AdminPage/>}/>
           <Route path = "/test" element={<TestPage/>}/>
+          <Route path = "/settings" element={<UserSettings/>}/>
           
         </Routes>
 
